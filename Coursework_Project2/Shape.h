5b569d1cf738e5e49f02e0e16ca1b3a264b20e7d
Thomas Hutchinson <thomashutchinson96@gmail.com>
2017-11-17 10:43:40 +0000

    init

---
+++ Coursework_Project2/Shape.h
@@ -0,0 +1,18 @@
+#pragma once
+
+/*
+	Author : Thomas Hutchinson
+	File   : Shape.h
+	Purpose: Programming for Games - Coursework 2
+	Description:
+		An abstract base class that provides a base level of implimentation rules for all
+		types that fall under a shape
+*/
+
+class Shape {
+public:
+
+
+private:
+
+};
